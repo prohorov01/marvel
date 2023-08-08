8d61030c684ff792f897b7e3ffc97fab901fab11
Alex <alexprohorov01@gmail.com>
2023-08-08 19:19:59 +0300

    update char info

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,36 +1,52 @@
 import { PureComponent } from "react";
-import mjolnir from "../../resources/img/mjolnir.png";
 import Spinner from "../spinner/Spinner";
-import MarvelService from "../../services/MarvelService";
 import ErrorMessage from "../errorMessage/ErrorMessage";
+import MarvelService from "../../services/MarvelService";
 
 import "./randomChar.scss";
+import mjolnir from "../../resources/img/mjolnir.png";
 
-export class RandomChar extends PureComponent {
-  f;
-  constructor(props) {
-    super(props);
-    this.updateChar();
-  }
-
+class RandomChar extends PureComponent {
   state = {
     char: {},
     loading: true,
     error: false,
   };
+
   marvelService = new MarvelService();
 
+  componentDidMount() {
+    this.updateChar();
+    // this.timerId = setInterval(this.updateChar, 15000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+
   onCharLoaded = (char) => {
-    this.setState({ char, loading: false });
+    this.setState({
+      char,
+      loading: false,
+    });
   };
+
+  onCharLoading = () => {
+    this.setState({
+      loading: true,
+    });
+  };
+
   onError = () => {
     this.setState({
       loading: false,
       error: true,
     });
   };
+
   updateChar = () => {
     const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+    this.onCharLoading();
     this.marvelService
       .getCharacter(id)
       .then(this.onCharLoaded)
@@ -55,7 +71,7 @@ export class RandomChar extends PureComponent {
             Do you want to get to know him better?
           </p>
           <p className="randomchar__title">Or choose another one</p>
-          <button className="button button__main">
+          <button onClick={this.updateChar} className="button button__main">
             <div className="inner">try it</div>
           </button>
           <img src={mjolnir} alt="mjolnir" className="randomchar__decoration" />
@@ -67,10 +83,22 @@ export class RandomChar extends PureComponent {
 
 const View = ({ char }) => {
   const { name, description, thumbnail, homepage, wiki } = char;
+  let imgStyle = { objectFit: "cover" };
+  if (
+    thumbnail ===
+    "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+  ) {
+    imgStyle = { objectFit: "contain" };
+  }
 
   return (
     <div className="randomchar__block">
-      <img src={thumbnail} alt="Random character" className="randomchar__img" />
+      <img
+        src={thumbnail}
+        alt="Random character"
+        className="randomchar__img"
+        style={imgStyle}
+      />
       <div className="randomchar__info">
         <p className="randomchar__name">{name}</p>
         <p className="randomchar__descr">{description}</p>
