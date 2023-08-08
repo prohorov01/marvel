cdee0f27a0a2e8d1dfefaeb07e50dd8a68134bda
Alex <alexprohorov01@gmail.com>
2023-08-08 23:04:37 +0300

    update error boundary, add char list

---
+++ src/components/errorBoundary/ErrorBoundary.js
@@ -0,0 +1,25 @@
+import { PureComponent } from "react";
+import ErrorMessage from "../errorMessage/ErrorMessage";
+
+class ErrorBoundary extends PureComponent {
+  state = {
+    error: false,
+  };
+
+  componentDidCatch(error, errorInfo) {
+    console.log(error, errorInfo);
+    this.setState({
+      error: true,
+    });
+  }
+
+  render() {
+    if (this.state.error) {
+      return <ErrorMessage />;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,7 @@
 class MarvelService {
   _apiBase = "https://gateway.marvel.com:443/v1/public/";
   _apiKey = "apikey=a9fcceddf72b2bba2170a4db6fd36de0";
+  _baseoffset = 210;
 
   getResource = async (url) => {
     let res = await fetch(url);
@@ -12,9 +13,9 @@ class MarvelService {
     return await res.json();
   };
 
-  getAllCharacters = async () => {
+  getAllCharacters = async (offset = this._baseoffset) => {
     const res = await this.getResource(
-      `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
+      `${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`
     );
     return res.data.results.map(this._transformCharacter);
   };
